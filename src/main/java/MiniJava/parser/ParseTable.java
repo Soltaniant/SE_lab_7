dc17345fb3744e838ee83f81bd8102f42632fddf
s.soltanian <s.soltaniant@gmail.com>
2024-12-21 22:26:40 +0330

    #4 extract method

--- src/main/java/MiniJava/parser/ParseTable.java
+++ src/main/java/MiniJava/parser/ParseTable.java
@@ -1,41 +1,35 @@
 package MiniJava.parser;
 
-import MiniJava.errorHandler.ErrorHandler;
 import MiniJava.scanner.token.Token;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by mohammad hosein on 6/25/2015.
- */
-
 public class ParseTable {
+
     private ArrayList<Map<Token, Action>> actionTable;
     private ArrayList<Map<NonTerminal, Integer>> gotoTable;
 
     public ParseTable(String jsonTable) throws Exception {
+        actionTable = new ArrayList<Map<Token, Action>>();
+        gotoTable = new ArrayList<Map<NonTerminal, Integer>>();
+        init(jsonTable);
+    }
+
+    private void init(String jsonTable) throws Exception {
         jsonTable = jsonTable.substring(2, jsonTable.length() - 2);
         String[] Rows = jsonTable.split("\\],\\[");
         Map<Integer, Token> terminals = new HashMap<Integer, Token>();
         Map<Integer, NonTerminal> nonTerminals = new HashMap<Integer, NonTerminal>();
         Rows[0] = Rows[0].substring(1, Rows[0].length() - 1);
         String[] cols = Rows[0].split("\",\"");
-        for (int i = 1; i < cols.length; i++) {
-            if (cols[i].startsWith("Goto")) {
-                String temp = cols[i].substring(5);
-                try {
-                    nonTerminals.put(i, NonTerminal.valueOf(temp));
-                } catch (Exception e) {
-                    ErrorHandler.printError(e.getMessage());
-                }
-            } else {
-                terminals.put(i, new Token(Token.getTyepFormString(cols[i]), cols[i]));
-            }
-        }
-        actionTable = new ArrayList<Map<Token, Action>>();
-        gotoTable = new ArrayList<Map<NonTerminal, Integer>>();
+        setTerminalsAndCols(terminals, nonTerminals, cols);
+        initTables(Rows, terminals, nonTerminals);
+    }
+
+    private void initTables(String[] Rows, Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals) throws Exception {
+        String[] cols;
         for (int i = 1; i < Rows.length; i++) {
             if (i == 100) {
                 int a = 1;
@@ -50,13 +44,9 @@ public class ParseTable {
                     if (cols[j].equals("acc")) {
                         actionTable.get(actionTable.size() - 1).put(terminals.get(j), new Action(act.accept, 0));
                     } else if (terminals.containsKey(j)) {
-//                        try {
                         Token t = terminals.get(j);
                         Action a = new Action(cols[j].charAt(0) == 'r' ? act.reduce : act.shift, Integer.parseInt(cols[j].substring(1)));
                         actionTable.get(actionTable.size() - 1).put(t, a);
-//                        }catch (StringIndexOutOfBoundsException e){
-//                            e.printStackTrace();
-//                        }
                     } else if (nonTerminals.containsKey(j)) {
                         gotoTable.get(gotoTable.size() - 1).put(nonTerminals.get(j), Integer.parseInt(cols[j]));
                     } else {
@@ -67,14 +57,22 @@ public class ParseTable {
         }
     }
 
+    private static void setTerminalsAndCols(Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals, String[] cols) {
+        for (int i = 1; i < cols.length; i++) {
+            if (cols[i].startsWith("Goto")) {
+                String temp = cols[i].substring(5);
+                try {
+                    nonTerminals.put(i, NonTerminal.valueOf(temp));
+                } catch (Exception e) {
+                    temp = temp;
+                }
+            } else {
+                terminals.put(i, new Token(Token.getTyepFormString(cols[i]), cols[i]));
+            }
+        }
+    }
     public int getGotoTable(int currentState, NonTerminal variable) {
-//        try {
         return gotoTable.get(currentState).get(variable);
-//        }catch (NullPointerException dd)
-//        {
-//            dd.printStackTrace();
-//        }
-//        return 0;
     }
 
     public Action getActionTable(int currentState, Token terminal) {
