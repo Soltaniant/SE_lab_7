d8262a5d2e7cd44ae126a03a1cb31ca19e8ee7e5
s.soltanian <s.soltaniant@gmail.com>
2024-12-21 21:56:23 +0330

    #3: self encapsulated field

--- src/main/java/MiniJava/codeGenerator/Memory.java
+++ src/main/java/MiniJava/codeGenerator/Memory.java
@@ -1,5 +1,8 @@
 package MiniJava.codeGenerator;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 
 /**
@@ -66,36 +69,35 @@ public class Memory {
             System.out.println(i + " : " + getCodeBlock().get(i).toString());
         }
     }
-}
 
-class _3AddressCode {
-    public Operation operation;
-    public Address Operand1;
-    public Address Operand2;
-    public Address Operand3;
+    @Getter
+    @NoArgsConstructor
+    class _3AddressCode {
+        private Operation operation;
+        private Address Operand1;
+        private Address Operand2;
+        private Address Operand3;
 
-    public _3AddressCode() {
+        public _3AddressCode(Operation op, Address opr1, Address opr2, Address opr3) {
+            operation = op;
+            Operand1 = opr1;
+            Operand2 = opr2;
+            Operand3 = opr3;
+        }
 
+        public String toString() {
+            if (operation == null) return "";
+            StringBuffer res = new StringBuffer("(");
+            res.append(operation.toString()).append(",");
+            if (getOperand1() != null) res.append(getOperand1());
+            res.append(",");
+            if (getOperand2() != null) res.append(getOperand2());
+            res.append(",");
+            if (getOperand3() != null) res.append(getOperand3());
+            res.append(")");
+
+            return res.toString();
+        }
     }
 
-    public _3AddressCode(Operation op, Address opr1, Address opr2, Address opr3) {
-        operation = op;
-        Operand1 = opr1;
-        Operand2 = opr2;
-        Operand3 = opr3;
-    }
-
-    public String toString() {
-        if (operation == null) return "";
-        StringBuffer res = new StringBuffer("(");
-        res.append(operation.toString()).append(",");
-        if (Operand1 != null) res.append(Operand1.toString());
-        res.append(",");
-        if (Operand2 != null) res.append(Operand2.toString());
-        res.append(",");
-        if (Operand3 != null) res.append(Operand3.toString());
-        res.append(")");
-
-        return res.toString();
-    }
 }
